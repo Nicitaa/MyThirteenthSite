55b31fa72877dbb41eb6d2f15d948c72b891ff3d
Nicitaa <icpcsenondaryemail@gmail.com>
2023-04-21 11:02:04 +0200

    1)Erstellung der Componentes

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react"
+import TodoList from "./components/TodoList"
+import Todo from "./components/Todo"
 
+const todos = [
+  {description:'Einkaufen',done:true},
+  {description:'Sport',done:false},
+  {description:'Programmieren',done:false}
+]
 
 function App() {
   return (
     <>
-    <h1>Hello React</h1>
+    <h1>Unsere todos</h1>
+    <Todo/>
+    <Todo/>
+    <Todo/>
     </>
   )
 }
