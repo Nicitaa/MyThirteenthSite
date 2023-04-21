db090a5ded52d04fd1a4585afc89bf76861fa897
Nicitaa <icpcsenondaryemail@gmail.com>
2023-04-21 16:23:31 +0200

    5)useState hook

--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 const Todo = ({desc,done}) => {
+
+    const [isDone,setIsDone] = useState(done)
+    const changeTodo = () => { 
+        setIsDone(isDone ? false : true) 
+    }
     return (
-      <div className={done ? 'd-flex justify-between items-center p-2 border-b-2 shadow-md bg-green-500' :
+      <div className={isDone ? 'd-flex justify-between items-center p-2 border-b-2 shadow-md bg-green-500' :
       'd-flex justify-between items-center p-2 shadow-md border-b-2 bg-red-500'}>
         <div className="flex justify-between items-center p-2">
-            <h1 className="text-lg cursor-pointer">{desc}</h1>
+            <h1 className="text-lg cursor-pointer" onClick={changeTodo}>{desc}</h1>
             <button className="text-lg bg-gray-400 p-2 text-white">Loschen</button>
         </div>
         </div>
