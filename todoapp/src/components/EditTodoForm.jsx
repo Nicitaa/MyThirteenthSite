35c0a152a57b5d4e93944e54cfb8a6e79e43bbf5
Nicitaa <icpcsenondaryemail@gmail.com>
2023-04-22 11:18:50 +0200

    5)Improved

--- todoapp/src/components/EditTodoForm.jsx
+++ todoapp/src/components/EditTodoForm.jsx
@@ -1,5 +1,20 @@
-export function EditTodoFrom () {
+import { useState } from "react"
+
+export function EditTodoFrom ({editTodo,todo}) {
+    const [value,setValue] = useState(todo.title)
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        editTodo(value,todo.id)
+
+        setValue('')
+    }
+    
     return (
-    <div></div>
+    <form className="TodoFrom" onSubmit={handleSubmit}>
+        <input value={value} onChange={e => setValue(e.target.value)} className="todo-input"
+        placeholder="Edit todo" />
+        <button className="todo-btn" type="submit">Edit</button>
+    </form>
     )
     }
--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -1,12 +1,12 @@
 import { faPenSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export function Todo ({todo,toggleComplete,deleteTodo}) {
+export function Todo ({todo,toggleComplete,deleteTodo,showEdit}) {
     return (
     <div className="Todo">
         <p className={`${todo.completed ? 'completed' : ''}`} onClick={() => toggleComplete(todo.id)}>{todo.title}</p>
         <div>
-            <FontAwesomeIcon icon={faPenSquare} />
+            <FontAwesomeIcon icon={faPenSquare} onClick={() => showEdit(todo.id)}/>
             <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(todo.id)}/>
         </div>
     </div>
